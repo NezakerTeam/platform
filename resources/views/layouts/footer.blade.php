ec4900f87ff26b42154430f13237ea87be78b8e2
Sherif Medhat <sherif.mohamed.medhat@gmail.com>
2016-12-24 01:43:31 +0200

    Add: Homepage template

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,62 @@
+<!--Bottom Four Column start-->
+<section class="blue_section section_gap">
+    <div class="container">
+        <div class="row bottomfourcol"> 
+            <!-- Footer About us start-->
+            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 bottomAbout">
+                <h5 class="heading">حولنا</h5>
+                <p>Phasellus ماتيس الهرية quis ENIM viverratys accumsan. Nullam RISUS بورتا الهرية، vitaeuik dapibus arcu viverra الاتحاد الأوروبي.</p>
+                <h5>نحن على الاجتماعي</h5>
+                <div class="socialshare"> 
+                    <a href="#"><i class="fa fa-twitter"></i></a>
+                    <a href="#"><i class="fa fa-facebook"></i></a>
+                    <a href="#"><i class="fa fa-google"></i></a>
+                    <a href="#"><i class="fa fa-vimeo-square"></i></a>
+                    <a href="#"><i class="fa fa-dribbble"></i></a>
+                    <a href="#"><i class="fa fa-youtube"></i></a> 
+                </div>
+            </div>
+            <!-- Footer About us end--> 
+            <!-- Footer Recent news start-->
+            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                <h5 class="heading">آخر الأخبار</h5>
+                <ul class="footerLinks">
+                    <li><a href="#">Standred الرد مع صورة مميزة</a><span>عن طريق <a href="#">مايك دو</a> في 24 يونيو 2014</span></li>
+                    <li><a href="#">Standred الرد مع صورة مميزة</a><span>عن طريق <a href="#">مايك دو</a> في 24 يونيو 2014</span></li>
+                    <li><a href="#">Standred الرد مع صورة مميزة</a><span>عن طريق <a href="#">مايك دو</a> في 24 يونيو 2014</span></li>
+                </ul>
+            </div>
+            <!-- Footer Recent news end--> 
+            <!-- Footer How it works start-->
+            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                <h5 class="heading">كيف تعمل ؟</h5>
+                <p>Phasellus ماتيس الهرية quis ENIM viverratys accumsan. Nullam RISUS بورتا الهرية، vitaeuik dapibus arcu viverra الاتحاد الأوروبي.</p>
+                <ul class="list">
+                    <li>Phasellus ماتيس الهرية quis ENIM</li>
+                    <li>Nullam بورتا RISUS vitaeuik dapibus </li>
+                    <li>Phasellus ماتيس الهرية quis ENIM </li>
+                    <li>Vivamus الجلوس امات ligulague سمبر</li>
+                </ul>
+            </div>
+            <!-- Footer How it works end--> 
+            <!-- Footer we accept start-->
+            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 contactInfo">
+                <h5 class="heading">نقبل</h5>
+                <p>Phasellus ماتيس الهرية quis ENIM viverratys accumsan. Nullam RISUS بورتا الهرية، vitaeuik dapibus arcu viverra الاتحاد الأوروبي.</p>
+                <ul>
+                    <li><img src="images/paypal.gif" width="31" height="22" alt=""></li>
+                    <li><img src="images/amazon.gif" width="31" height="22" alt=""></li>
+                    <li><img src="images/visa.gif" width="31" height="22" alt=""></li>
+                    <li><img src="images/master.gif" width="31" height="22" alt=""></li>
+                    <li><img src="images/discover.gif" width="31" height="22" alt=""></li>
+                </ul>
+            </div>
+            <!-- Footer we accept end--> 
+        </div>
+    </div>
+</section>
+<!--/Bottom Four Column end --> 
+
 <!--Footer start-->
 <footer class="footer">
     <div class="container">
@@ -5,7 +64,9 @@
             <div class="pull-left">
                 <p>&copy; 2015  eLearn Theme. All Rights Reserved</p>
             </div>
-            <div class="pull-right"><a class="gototop smooth" href="#wrapper">Go To Top<i class="fa fa-chevron-up"></i></a></div>
+            <div class="pull-right">
+                <a class="gototop smooth" href="#wrapper">Go To Top<i class="fa fa-chevron-up"></i></a>
+            </div>
         </div>
     </div>
 </footer>
