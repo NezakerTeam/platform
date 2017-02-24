9a08a96d543f37e4ca832fa733336eb13409a089
Sherif Medhat <sherif.mohamed.medhat@gmail.com>
2017-02-25 00:49:34 +0200

    Fix: The admin user CRUD

--- app/Models/Repositories/CityRepository.php
+++ app/Models/Repositories/CityRepository.php
@@ -1,6 +1,9 @@
 <?php
 namespace App\Models\Repositories;
 
+use App\Models\City;
+use Illuminate\Database\Eloquent\Collection;
+
 class CityRepository extends EntityRepository
 {
 
@@ -9,9 +12,16 @@ class CityRepository extends EntityRepository
      */
     protected static function getModel()
     {
-        return new \App\Models\City();
+        return new City();
     }
 
+    /**
+     * Get all the cities for a specific or all countries
+     * 
+     * @param int|null $countryId   The specific country id
+     * 
+     * @return Collection
+     */
     public static function getAll($countryId = null)
     {
         $citiesQB = self::getModel();
