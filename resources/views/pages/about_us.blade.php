13e9063466bfdfadd0a89c799388133c394f24a7
Sherif Medhat <sherif.mohamed.medhat@gmail.com>
2017-06-14 22:48:52 +0200

    Fix: Pages template

--- resources/views/pages/about_us.blade.php
+++ resources/views/pages/about_us.blade.php
@@ -3,16 +3,11 @@
 @section('content')
 
 <div class="container">
-    <div class="row">
-        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 watch_text">
-            <section id="aboutus">
-                <div class="heading">
-                    <h1><span>{{$page['title']}}</span></h1>
-                    {!! $page['content'] !!}
-                </div>
-        </div>
-    </div>
-</div>
+    <div class="heading">
+        <h1><span>{{$page['title']}}</span></h1>
+    </div>   
+    {!! $page['content'] !!}
 
+</div>
 
 @endsection
