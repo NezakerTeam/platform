3b4867bde7f77bdfce4e16868198b1e3194a6d8e
Sherif Medhat <sherif.mohamed.medhat@gmail.com>
2018-02-24 02:39:55 +0200

    Fix: Admin middleware

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -23,8 +23,7 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $router = app('router');
-        $router->middleware('admin', \App\Http\Middleware\Admin::class);
+        Route::aliasMiddleware('admin', \App\Http\Middleware\Admin::class);
 
         parent::boot();
     }
@@ -54,7 +53,7 @@ class RouteServiceProvider extends ServiceProvider
     {
         Route::group([
             'middleware' => 'web',
-            'namespace' => $this->namespace,
+            'namespace'  => $this->namespace,
             ], function ($router) {
             require base_path('routes/web.php');
         });
@@ -71,8 +70,8 @@ class RouteServiceProvider extends ServiceProvider
     {
         Route::group([
             'middleware' => 'api',
-            'namespace' => $this->namespace,
-            'prefix' => 'api',
+            'namespace'  => $this->namespace,
+            'prefix'     => 'api',
             ], function ($router) {
             require base_path('routes/api.php');
         });
